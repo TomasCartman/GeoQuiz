3bd5d11364239e818517241910a4299bce1e03a6
Tomas Cartman <marco12y@outlook.com>
2020-12-22 19:11:56 -0300

    Chapter 03 + challenges GeoQuiz

--- app/src/main/java/com/dashboarder/geoquiz/MainActivity.kt
+++ app/src/main/java/com/dashboarder/geoquiz/MainActivity.kt
@@ -2,12 +2,14 @@ package com.dashboarder.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton : Button
@@ -17,12 +19,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var questionTextView : TextView
 
     private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true)
+        Question(R.string.question_australia, true, false, false),
+        Question(R.string.question_oceans, true, false, false),
+        Question(R.string.question_mideast, false, false, false),
+        Question(R.string.question_africa, false, false, false),
+        Question(R.string.question_americas, true, false, false),
+        Question(R.string.question_asia, true, false, false)
     )
 
     private var currentIndex = 0
@@ -30,6 +32,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate(Bundle?) called")
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
@@ -48,8 +52,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         nextImageButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
-            updateQuestion()
+            if(currentIndex < questionBank.size  - 1) {
+                currentIndex += 1
+                updateQuestion()
+            }
         }
 
         previousImageButton.setOnClickListener {
@@ -60,28 +66,93 @@ class MainActivity : AppCompatActivity() {
         }
 
         questionTextView.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
-            updateQuestion()
+            if(currentIndex < questionBank.size  - 1) {
+                currentIndex += 1
+                updateQuestion()
+            }
         }
 
         updateQuestion()
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
+        checkButtonIsEnable()
+    }
+
+    private fun checkButtonIsEnable() {
+        if(questionBank[currentIndex].userHasAnswered) {
+            trueButton.isEnabled = false
+            falseButton.isEnabled = false
+        } else {
+            trueButton.isEnabled = true
+            falseButton.isEnabled = true
+        }
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
+        val correctAnswer = questionBank[currentIndex].questionAnswer
+        var messageResId = 0
 
-        val messageResId = if(userAnswer == correctAnswer) {
-            R.string.correct_toast
+        if(userAnswer == correctAnswer) {
+            questionBank[currentIndex].userHasAnswered = true
+            questionBank[currentIndex].userIsCorrect = true
+            messageResId = R.string.correct_toast
         } else {
-            R.string.incorrect_toast
+            questionBank[currentIndex].userHasAnswered = true
+            questionBank[currentIndex].userIsCorrect = false
+            messageResId = R.string.incorrect_toast
         }
 
+        checkButtonIsEnable()
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
             .show()
+
+        var count = questionBank.size
+        var score = 0
+        for (question in questionBank) {
+            if(question.userHasAnswered) {
+                count -= 1
+                if(question.userIsCorrect) {
+                    score += 1
+                }
+            }
+        }
+        if(count == 0) {
+            val percent = score.toDouble()/questionBank.size * 100
+            Toast.makeText(this, "Your score was: $percent%", Toast.LENGTH_LONG)
+                .show()
+        }
     }
 }
--- app/src/main/java/com/dashboarder/geoquiz/Question.kt
+++ app/src/main/java/com/dashboarder/geoquiz/Question.kt
@@ -2,4 +2,5 @@ package com.dashboarder.geoquiz
 
 import androidx.annotation.StringRes
 
-data class Question(@StringRes val textResId: Int, val answer: Boolean)
+data class Question(@StringRes val textResId: Int, val questionAnswer: Boolean,
+                    var userHasAnswered: Boolean, var userIsCorrect: Boolean)
